4072b12b3430c5d49d9ef41613e6e7c0846f3edb
Superior5 <superiorunknown5@gmail.com>
2021-12-12 00:18:14 +0300

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>Mazadaran Mansur Project</h1>
+    <h1>У моего проекта день рождения</h1>
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
