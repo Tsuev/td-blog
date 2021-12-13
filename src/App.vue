359f24979ee7da8737212e8a520bddd9bdc56311
Superior5 <superiorunknown5@gmail.com>
2021-12-14 00:07:35 +0300

    Make component td-blog-card.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 
       <div class="main">
         <td-header />
-
+  
         <main class="content p-0">
           <router-view />
         </main>
@@ -17,12 +17,20 @@
 <script>
 import tdSidebar from "@/components/sidebar/td-sidebar.vue";
 import TdHeader from "./components/header/td-header.vue";
+import tdCard from "./components/card/td-blog-card.vue";
+
 export default {
   components: {
     tdSidebar,
     TdHeader,
+    tdCard,
   },
 };
 </script>
 <style lang="scss">
+.main-card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
---
+++ src/components/card/td-blog-card.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="td-card">
+    <div class="card">
+      <div class="card-header">
+        <h5 class="card-title mb-0">{{ title }}</h5>
+      </div>
+      <div class="card-body">
+        <p class="card-text">
+          {{ description }}
+        </p>
+        <a href="/" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "td-card",
+  props: {
+    title: {
+      type: String,
+      default: "Заголовок",
+    },
+    description: {
+      type: String,
+      default:
+        "Описание Описание Описание Описание Описание Описание Описание Описание Описание ",
+    },
+    route: {
+      type: String,
+      default: "/card",
+    },
+  },
+};
+</script>
+
+<style>
+</style>
